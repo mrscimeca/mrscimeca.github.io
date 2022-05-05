746899077c3a7127c839c98b034195da7f6aeb6e
Ralph Scimeca <rscimeca@rtnj.org>
2022-05-05 12:02:32 -0400

    Update target_sketch.js

--- target/target_sketch.js
+++ target/target_sketch.js
@@ -1630,8 +1630,8 @@ windowResized = None
 `;
 
 let userCode = `
-from ball import Ball
-from enemy import Enemy
+
+
 
 enemy1 = []
 points = 0
@@ -1705,24 +1705,149 @@ def mousePressed():
     startGame = True
     ball1.fire()
     
-# def keyPressed():
-#     keyPr = 0
-    
-#     if key == "a" or key == "A":
-#         keyPr = "a"
+	
+	
+class Enemy(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.img = loadImage("alien.png")
+        self.h = 32
+        self.w = 32
+    def update(self):
+        self.move()
+
         
-#     if key == "s" or key == "S":
-#         keyPr = "s"
+    def move(self):
+        self.x += random(-1,0)
+        self.y += random(-2,2)
     
-#     if key == "w" or key == "W":
-#         keyPr = "w"
+    def isCollision(self, b):
+        if b.x + b.w > self.x and b.x - b.w < self.x + self.w and b.y > self.y - self.h/2 and b.y < self.y + self.h:
+            return True
+        else:
+            return False
         
-#     if key == "d" or key == "D":
-#         keyPr = "d"
+        
+        # for bull in bullist:
+            
+        #     if bull.x > self.x:
+        #         return True
+            
+        #     else:
+        #         return False
+        
+        
+        
+    def render(self):
+        image(self.img, self.x - self.w / 2 , self.y - self.h / 2)
+        # fill(150, 150, 0)
+        # rect(self.x, self.y, 15, 15)
+        
     
-#     ball1.sendKeyDown(keypr)
+class Ball(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speedx = 0
+        self.speedy = 0
+        self.bullet = []
+        self.img = loadImage("ship.png")
+        self.w = 32
+        self.h = 32
         
-                  
+        self.kPressed = { "w":0, 
+                            "a":0,
+                            "s":0,
+                            "d":0}
+        
+        
+    def update(self):
+        
+        
+        
+        if keyPressed == True:
+ 
+                
+            if key == "w" or key == "W":
+                self.speedy -= 1
+    
+            if key == "s" or key == "S":
+                self.speedy += 1
+            
+            if key == "a" or key == "A":
+                self.speedx -= 1
+            
+            if key == "d" or key == "D":
+                self.speedx += 1
+        
+        
+        
+        #Update x and y based on speed
+        self.x += self.speedx
+        self.y += self.speedy
+        
+        self.checkIfWall()
+        
+        #Apply friction
+        self.speedx *= 0.95
+        self.speedy *= 0.95
+        
+        for b in self.bullet:
+            b.update()
+            if b.x > 1280:
+                self.bullet.remove(b)
+        
+    def render(self):
+        
+        for b in self.bullet:
+            b.render()
+           
+        image(self.img, self.x - (self.w / 2), self.y - (self.h / 2))   
+             
+        # fill(255)
+        # ellipse(self.x, self.y, 10,10)
+        
+        
+        
+    def checkIfWall(self):
+        
+        
+        if self.y < 10:
+            self.y = 12
+            self.speedy = 1
+            
+        if self.y > 720:
+            self.y = 715
+            self.speedy = -1
+            
+        if self.x < 10:
+            self.x = 12
+            self.speedx = 1
+            
+        if self.x > 1000:
+            self.x = 995
+            self.speedx = -1
+    
+    def fire(self):
+        self.bullet.append( Bullet(self.x, self.y))
+        
+class Bullet(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speedx = 40
+        self.w = 20
+        self.h = 4
+        
+    def update(self):
+        self.x += self.speedx
+    
+
+        
+    def render(self):
+        fill(255, 100,100)
+        ellipse(self.x, self.y, self.w, self.h)
 `;
 
 const startCode = `
